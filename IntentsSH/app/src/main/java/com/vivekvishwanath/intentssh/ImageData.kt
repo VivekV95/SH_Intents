7e3ded003ee186636cc4aa96f8898b542f3b29a1
VivekV95 <vivekv210@gmail.com>
2019-08-09 08:58:44 -0500

    Create ImageData model

---
+++ IntentsSH/app/src/main/java/com/vivekvishwanath/intentssh/ImageData.kt
@@ -0,0 +1,16 @@
+package com.vivekvishwanath.intentssh
+
+import android.net.Uri
+import java.io.Serializable
+
+class ImageData(uri: Uri?, var name: String = ""): Serializable {
+    private var uriString = uri.toString()
+
+    fun getUri(): Uri {
+        return Uri.parse(uriString)
+    }
+
+    fun setUri(uri: Uri) {
+        uriString = uri.toString()
+    }
+}
