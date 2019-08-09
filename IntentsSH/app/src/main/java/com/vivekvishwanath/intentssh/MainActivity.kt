a7d80664b7a5af9b9dfd64508defd477a3dc72b1
VivekV95 <vivekv210@gmail.com>
2019-08-09 08:27:44 -0500

    Create MainActivity xml

--- IntentsSH/app/src/main/java/com/vivekvishwanath/intentssh/MainActivity.kt
+++ IntentsSH/app/src/main/java/com/vivekvishwanath/intentssh/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.vivekvishwanath.intentssh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val ADD_IMAGE_CODE = 10
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_movie_button.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivityForResult(intent, ADD_IMAGE_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
